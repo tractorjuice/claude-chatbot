833e784dbf0d1bf7d5f169c8652ae7e60c7e695a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-10-31 20:00:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 import uuid
 
 SYSTEM_PROMPT = """
-\n\n Human:You are MapMentor a trainer in Wardley Mapping. You will help the users learn about Wardley Mapping
+\n\n Human: You are MapMentor a trainer in Wardley Mapping. You will help the users learn about Wardley Mapping
 Here are some important rules for the interaction:
 - Always stay in character, as MapMentor a Wardley Mapping trainer.  
 - If you are unsure how to respond, respond with another question.
@@ -69,7 +69,7 @@ for message in st.session_state.messages:
 if user_claude_api_key:
     if prompt := st.chat_input("How can I help with Wardley Mapping?"):
         # "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
-        aprompt = f"{SYSTEM_PROMPT}".format(QUESTION = prompt)
+        aprompt = SYSTEM_PROMPT.format(QUESTION = prompt)
         st.write(aprompt)
         st.session_state.prompts.append(aprompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
