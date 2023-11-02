18b18e312f28dfb7672a0bb1069cfb902f5233bf
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-11-02 07:51:31 +0000

    Update app.py

--- app.py
+++ app.py
@@ -163,6 +163,7 @@ if user_claude_api_key:
         st.session_state.messages.append({"role": "assistant", "content": full_response})
         st.session_state.all_prompts += full_response
         prompt_token_count, completion_token_count, total_cost = count_used_tokens(prompt, full_response)
-        total_tokens.write("Total:" + str(prompt_token_count))
-        total_tokens.write("Completiioon: " + str(completion_token_count))
-        total_tokens.write("Total Cost: " + str(total_cost))
+        with total_tokens:
+            st.write("Total:" + str(prompt_token_count))
+            st..write("Completiioon: " + str(completion_token_count))
+            st.write("Total Cost: " + str(total_cost))
